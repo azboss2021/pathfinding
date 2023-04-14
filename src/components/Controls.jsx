353c2918179b0cdce482cab10d681e7784687a46
azboss2021 <alexanderzyn1@gmail.com>
2023-04-13 23:08:56 -0700

    Adjusted control UI

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -2,15 +2,15 @@ import { FaTrash, FaPlay } from 'react-icons/fa';
 
 const Controls = ({ handlePlay, handleClear }) => {
   return (
-    <section className="absolute top-0 left-10 flex gap-3 bg-white border-2 border-gray-300 rounded-b-lg border-t-0 p-4">
+    <section className="absolute top-0 left-10 flex gap-3 bg-white border-2 border-gray-300 rounded-b-lg border-t-0 p-3">
       <button
-        className="w-15 aspect-square p-4 text-white bg-blue-400 rounded-md control-button"
+        className="w-15 aspect-square p-3 text-white bg-blue-400 rounded-md control-button"
         onClick={handlePlay}
       >
         <FaPlay />
       </button>
       <button
-        className="w-15 aspect-square p-4 text-white bg-blue-400 rounded-md control-button"
+        className="w-15 aspect-square p-3 text-white bg-blue-400 rounded-md control-button"
         onClick={handleClear}
       >
         <FaTrash />
